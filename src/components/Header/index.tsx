85eb0e5c6d2f69f1f2300eaa77b5f898351d07ec
Nikolas <nikolaspg@hotmail.com>
2021-04-22 19:15:34 -0300

    aula3

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
 
+import Link from 'next/link'
+
 
 import styles from './styles.module.scss';
 
@@ -14,7 +16,9 @@ export function Header() {
 
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcsatr" />
+      <Link href="/">
+      <img src="/logo.svg" alt="Podcastr" />
+      </Link>
 
       <p>O melhor para você ouvir, sempre</p>
 
