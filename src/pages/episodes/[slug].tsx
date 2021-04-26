ea7e01808f2c3ab650024d7c7da7c03da3b4b260
Nikolas <nikolaspg@hotmail.com>
2021-04-26 10:47:57 -0300

    aula04

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,12 +2,13 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import { api } from '../../services/api';
-// import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 import styles from './episode.module.scss';
+import { Head } from 'next/document';
 
 
 type Episode = {
@@ -26,14 +27,22 @@ type EpisodeProps = {
   episode: Episode;
 }
 export default function Episode({ episode }: EpisodeProps) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <div className={styles.episode}>
+      {/* <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head> */}
       <div className={styles.thumbnailContainer}>
         <Link href="/">
-        <button type="button">
-          <img src="/arrow-left.svg" alt="Voltar" />
-        </button>
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar" />
+          </button>
         </Link>
         <Image width={700} height={160} src={episode.thumbnail} objectFit='cover' />
         <button type="button">
@@ -41,30 +50,45 @@ export default function Episode({ episode }: EpisodeProps) {
         </button>
       </div>
 
-    <header>
-      <h1>{episode.title}</h1>
-      <span>{episode.members}</span>
-      <span>{episode.publishedAt}</span>
-      <span>{episode.durationAsString}</span>
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
 
-      <div className={styles.description} dangerouslySetInnerHTML={{__html: episode.description}}/>
-    </header>
+      </header>
+      <div className={styles.description} dangerouslySetInnerHTML={{ __html: episode.description }} />
 
     </div>
   )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      __limit: 2,
+      __sort: 'published_at',
+      __order: 'desc'
+    }
+  })
+
+  const paths = data.map(episode => ({
+    params: {
+      slug: episode.id,
+    }
+  }))
+  console.log(paths)
+
   return {
-    paths: [],
-    fallback: 'blocking'
+    paths,
+    fallback: true
   }
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { slug } = ctx.params;
 
-  const { data } = await api.get(`episodes/${slug}`)
+  const { data } = await api.get(`/episodes/${slug}`)
 
   const episode = {
     id: data.id,
@@ -81,7 +105,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 
   return {
     props: {
-      episode,
+      episode
     },
     revalidate: 60 * 60 * 24, //24hrs
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,8 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 import styles from './home.module.scss'
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 
 type Episode = {
@@ -30,12 +32,13 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
-
+  const {play } = useContext(PlayerContext)
 
   return (
+
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
-        <h2>Últimos lançamentos</h2>
+        <h2>Últimos lançamentos </h2>
         <ul>
           {latestEpisodes.map(episode => {
             return (
@@ -51,7 +54,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
@@ -88,7 +91,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
                   </td>
                   <td>
-                    <Link href={`/episode/${episode.id}`}>
+                    <Link href={`/episodes/${episode.id}`}>
                       <a>{episode.title}</a>
                     </Link>
                   </td>
@@ -112,7 +115,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 }
 export const getStaticProps: GetStaticProps = async () => {
 
-  const { data } = await api.get('episodes', {
+  const { data } = await api.get('/episodes', {
     params: {
       _limit: 12,
       _sort: 'published_at',
